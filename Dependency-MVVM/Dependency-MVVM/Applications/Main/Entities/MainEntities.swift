1c7297d1450bd1b7e787d11bd35abe2672c11f9b
Do-hyun-Kim <dohyun8032@gmail.com>
2022-05-12 01:45:10 +0900

    MainViewModel에 View에 대한 Dependency를 구현하여 View에 대한 구현, 비즈니스 로직 의존성 분리

---
+++ Dependency-MVVM/Dependency-MVVM/Applications/Main/Entities/MainEntities.swift
@@ -0,0 +1,14 @@
+//
+//  MainEntities.swift
+//  Dependency-MVVM
+//
+//  Created by Kim dohyun on 2022/05/12.
+//
+
+import Foundation
+
+struct MainEntities {
+    var person: String
+    var age: Int
+    var type: String
+}
--- Dependency-MVVM/Dependency-MVVM/Applications/Main/ViewModel/MainViewModel.swift
+++ Dependency-MVVM/Dependency-MVVM/Applications/Main/ViewModel/MainViewModel.swift
@@ -6,11 +6,23 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
+
+protocol TableViewDependency {
+    func numberOfSections() -> Int
+    func showTableView(cellForRowAt: IndexPath)
+}
 
 final class MainViewModel {
     
     private var coordinator: AppCoordinator?
+    private var mainDependecy: TableViewDependency
+    
+    var numberOfRowInSection: Int {
+        return mainDependecy.numberOfSections()
+    }
     
     init(coordinator: AppCoordinator) {
         self.coordinator = coordinator
@@ -18,6 +30,21 @@ final class MainViewModel {
     
     public func didTapBackButton() {
         coordinator?.moveToLoginView()
+    }
+    
+    
+    
+    
+    
+}
+
+final class MainDependency: TableViewDependency {
+    func numberOfSections() -> Int {
+        <#code#>
+    }
+    
+    func showTableView(cellForRowAt: IndexPath) {
+        <#code#>
     }
     
 }
